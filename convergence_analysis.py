50971db4856319d703d7c12567074d9532d563da
Martin Stroet <martin.stroet@gmail.com>
2017-01-14 15:22:50 +1000

    Code refactor

--- convergence_analysis.py
+++ convergence_analysis.py
@@ -3,104 +3,98 @@ import numpy as np
 from ks_convergence.helpers import value_to_closest_index
 from ks_convergence.plot import create_figure, add_axis_to_figure
 
+def find_converged_blocks(test_region_sizes, ks_vals, p_vals, convergence_criteria, step_size):
+    def is_converged(x):
+        return x < convergence_criteria
+    converged_blocks = []
+    in_converged_block = False
+    for ks_value, p_value, test_region_size in zip(ks_vals, p_vals, test_region_sizes):
+        if in_converged_block and is_converged(ks_value):
+            converged_blocks[-1].append( (ks_value, p_value, test_region_size) )
+        elif is_converged(ks_value):
+            converged_blocks.append( [(ks_value, p_value, test_region_size)] )
+            in_converged_block = True
+        else:
+            in_converged_block = False
 
-def ks_convergence_analysis(x, y, convergence_criteria=0.95, step_size_in_percent=1):
+    # rearrange data
+    converged_blocks = [zip(*block) for block in converged_blocks]
 
-    stepSize = (x[-1]-x[0])*(step_size_in_percent/100.0)
-    stepIndex = value_to_closest_index(x, stepSize)
-    if stepIndex == 0:
+    converged_block_bounds = []
+    min_ks_values = []
+    max_p_values = []
+    for block_ks_vals, block_p_vals, block_test_region_sizes in converged_blocks:
+        converged_block_bounds.append( (block_test_region_sizes[0], block_test_region_sizes[-1]) )
+        min_ks_values.append( np.min(block_ks_vals) )
+        max_p_values.append( np.max(block_p_vals) )
+    return converged_block_bounds, min_ks_values, max_p_values
+
+def test_multiple_regions(x, y, step_index):
+    # length of test regions, ensure all value are considered by starting from len(x)
+    region_indexes = np.arange(len(x), 0, -step_index)[::-1]
+    # convert indexes into x values
+    test_region_sizes = [(x[-1] - x[-test_region_len]) for test_region_len in region_indexes]
+    # perform ks test on first and 2nd halves of each region
+    ks_vals, p_vals = zip(*[ks_test(y[-test_region_len:]) for test_region_len in region_indexes])
+
+    return test_region_sizes, ks_vals, p_vals
+
+def ks_convergence_analysis(x, y, convergence_criteria=0.05, step_size_in_percent=1):
+
+    step_size = (x[-1]-x[0])*(step_size_in_percent/100.0)
+    step_index = value_to_closest_index(x, step_size)
+    if step_index == 0:
         raise Exception("StepIndex = 0, this will cause infinite loop.")
 
-    regionSizeIndex = len(x)
-    ksval, pval = ks_test(y[-regionSizeIndex:])
+    test_region_sizes, ks_vals, p_vals = test_multiple_regions(x, y, step_index)
+    converged_blocks, block_min_ks_values, block_max_p_values = find_converged_blocks(test_region_sizes, ks_vals, p_vals, convergence_criteria, step_size)
+    if converged_blocks:
+        largest_converged_block, largest_converged_block_minimum_ks_value, largest_converged_block_minimum_p_value\
+            = sorted(zip(converged_blocks, block_min_ks_values, block_max_p_values), key=lambda x: x[0][1] - x[0][0])[-1]
+        minimum_sampling_time = largest_converged_block[0]
+        equilibration_time = x[-1] - largest_converged_block[1]
+    else:
+        minimum_sampling_time = 0
+        equilibration_time = x[-1]
+        largest_converged_block_minimum_ks_value = np.min(ks_vals)
+        largest_converged_block_minimum_p_value = np.min(p_vals)
 
-    regionSizeList = []
-    pVals = []
-    ksVals = []
-    while regionSizeIndex > 0:
-        regionSizeList.append(x[-1] - x[-regionSizeIndex])
-        pVals.append(pval)
-        ksVals.append(ksval)
-
-        regionSizeIndex -= stepIndex
-        # apply region size filter to the end of the run
-        ksval, pval = ks_test(y[-regionSizeIndex:])
-
-    convergedClusters = []
-    convergedKSVals = []
-    for i, ks in enumerate(ksVals):
-        if ks > convergence_criteria:
-            regionSize = regionSizeList[i]
-            # check to see if it's next to current cluster, first make sure there already is a region
-            if len(convergedClusters) > 0:
-                # if the difference between region sizes is greater than stepSize + some tolerance to allow for rounding of float
-                if abs((convergedClusters[-1][-1] - regionSize) - stepSize) > stepSize/2.0:
-                    # then it's not neighboring previous cluster, start a new one
-                    convergedClusters.append([regionSize])
-                    convergedKSVals.append([ks])
-                else:
-                    # it is neighboring previous cluster, so append it
-                    convergedClusters[-1].append(regionSize)
-                    convergedKSVals[-1].append(ks)
-            else:
-                convergedClusters.append([regionSize])
-                convergedKSVals.append([ks])
     fig = create_figure()
     ax_ks = add_axis_to_figure(fig, 211)
     ax_summary = add_axis_to_figure(fig, 212, sharex=ax_ks)
-    plot_figure(x, y, convergedClusters, convergence_criteria, step_size_in_percent, ax_ks, ax_summary)
 
-    return convergedClusters, convergedKSVals, fig
+    plot_figure(x, y, test_region_sizes, ks_vals, p_vals, equilibration_time, minimum_sampling_time, convergence_criteria, step_size_in_percent, ax_ks, ax_summary)
 
-def plot_figure(time, dvdl, convClusters, convergence_criteria, step_size_in_percent, ax_ks, ax_summary):
-    stepSize = (time[-1]-time[0])*(step_size_in_percent/100.0)
-    stepIndex = value_to_closest_index(time, stepSize)
+    return minimum_sampling_time, equilibration_time, largest_converged_block_minimum_ks_value, largest_converged_block_minimum_p_value, fig
 
-    regionSizeIndex = len(time)
-    ksval, pval = ks_test(dvdl[-regionSizeIndex:])
+def plot_figure(x, y, test_region_sizes, ks_values, p_values, equilibration_time, minimum_sampling_time, convergence_criteria, step_size, ax_ks, ax_summary):
 
-    regionSizeList = []
-    pVals = []
-    ksVals = []
-    while regionSizeIndex > 0:
-        regionSizeList.append(time[-1] - time[-regionSizeIndex])
-        pVals.append(pval)
-        ksVals.append(ksval)
-
-        regionSizeIndex -= stepIndex
-        # apply region size filter to the end of the run
-        ksval, pval = ks_test(dvdl[-regionSizeIndex:])
-
-    ax_ks.plot(regionSizeList, ksVals, linestyle='-',color="k",marker ='o')
-    ax_ks.plot([0, max(regionSizeList)], [convergence_criteria, convergence_criteria], linestyle='-',color="r", zorder=3)
+    ax_ks.plot(test_region_sizes, ks_values, linestyle='-',color="k",marker ='o')
+    ax_ks.plot(test_region_sizes, p_values, linestyle='-',color="g",marker ='.')
+    ax_ks.plot([0, max(test_region_sizes)], [convergence_criteria, convergence_criteria], linestyle='-',color="r", zorder=3)
     ax_ks.set_ylabel("1-ks statistic")
     ax_ks.set_xlabel("end anchored region size")
 
-    ax_summary.plot(time, dvdl, color="k",alpha=.5)
+    ax_summary.plot(x, y, color="k",alpha=.5)
 
-    if convClusters:
-        maxContCluster = []
-        maxContinuity = -1
-        for cluster in convClusters:
-            if cluster[0] - cluster[-1] > maxContinuity:
-                maxContCluster = cluster
-                maxContinuity = cluster[0] - cluster[-1]
+    # plot minimum sampling region
+    minimum_sampling_region_left_bound = value_to_closest_index(x, x[-1] - minimum_sampling_time)
+    block_mean_y = np.mean(y[minimum_sampling_region_left_bound:])
+    ax_summary.errorbar([x[-1] - minimum_sampling_time/2.], [block_mean_y], xerr=[minimum_sampling_time/2.], color ="g", marker ='o', linestyle='', zorder=4, linewidth=2)
 
-        # plot the largest converged region
-        midCluster = (time[-1] - maxContCluster[0]) + (maxContCluster[0])/2.0
-        clusterStartIndex = value_to_closest_index(time, time[-1] - maxContCluster[0])
-        meanCluster = np.mean(dvdl[clusterStartIndex:])
-        ax_summary.errorbar([midCluster],[meanCluster],xerr=[maxContCluster[0]/2.0], color ="g",marker ='o',linestyle='',zorder=4, linewidth=2)
-        # plot the continuity
-        clusterStartIndexSm = value_to_closest_index(time, time[-1] - maxContCluster[-1])
-        meanClusterSm = np.mean(dvdl[clusterStartIndexSm:])
-        ax_summary.errorbar([(time[-1] - maxContCluster[-1]) + (maxContCluster[-1])/2.0],[meanClusterSm],xerr=[maxContCluster[-1]/2.0], color ="b",marker ='o',linestyle='',zorder=4, linewidth=2)
+    # plot equilibration region
+    equilibration_time_right_bound = value_to_closest_index(x, equilibration_time)
+    equilibration_region_mean = np.mean(y[:equilibration_time_right_bound])
+    ax_summary.errorbar([equilibration_time/2.0],[equilibration_region_mean], xerr=[equilibration_time/2.0], color ="r", marker ='o', linestyle='', zorder=4, linewidth=2)
+
+    # plot equilibrated_region
+    equilibrated_region_mean = np.mean(y[equilibration_time_right_bound:])
+    ax_summary.errorbar([x[-1] - (x[-1] - equilibration_time)/2.0], [equilibrated_region_mean], xerr=[(x[-1] - equilibration_time)/2.0], color="b", marker ='o', linestyle='', zorder=3, linewidth=2)
 
     ax_summary.set_ylabel("y")
     ax_summary.set_xlabel("x")
     ax_summary.set_xlim(0)
 
-def ks_test(dvdl):
-    testdvdl, refDvdl = dvdl[:len(dvdl)/2], dvdl[len(dvdl)/2:]
-    ks, p = ks_2samp(testdvdl,refDvdl) 
-    return 1-ks, p
+def ks_test(x):
+    test_values, ref_values = x[:len(x)/2], x[len(x)/2:]
+    return ks_2samp(test_values, ref_values)
--- test/test.py
+++ test/test.py
@@ -1,12 +1,12 @@
 import sys
 import os
 from ks_convergence.plot import save_figure
-from ks_convergence.helpers import value_to_closest_index
+
 sys.path.append("../../")
 import numpy as np
 from ks_convergence.convergence_analysis import ks_convergence_analysis
 
-TEST_DATA = ["not_converged_test.dat","converged_test.dat", "converged_test_2.dat"]
+TEST_DATA = ["large_data_set.dat"]#["not_converged_test.dat","converged_test.dat", "converged_test_2.dat"]
 
 def sloppy_data_parser(file_name):
     with open(file_name) as fh:
@@ -15,21 +15,17 @@ def sloppy_data_parser(file_name):
 
 for data_path in TEST_DATA:
     x, y = sloppy_data_parser(data_path)
-    possible_converged_regions, ks_values, fig = ks_convergence_analysis(x, y)
+    minimum_sampling_time, equilibration_time, largest_converged_block_minimum_ks_value, largest_converged_block_minimum_p_value, fig = ks_convergence_analysis(x, y)
     fig.tight_layout()
     save_figure(fig, os.path.basename(data_path)[:-4])
 
-    maxContinuity = -1
-    maxContCluster = []
-    maxContKS = -1
-    for i, cluster in enumerate(possible_converged_regions):
-        if cluster[0] - cluster[-1] > maxContinuity:
-            maxContCluster = cluster
-            maxContinuity = cluster[0] - cluster[-1]
-            maxContKS = ks_values[i]
+    print "Equilibration time: {0:5.1f}".format(equilibration_time, )
+    print "minimum sampling time: {0:5.1f}".format(minimum_sampling_time, )
+    print "Equilibrated region: {0:5.1f}-->{1:5.1f}".format(equilibration_time, x[-1])
 
-    print "\nLargest converged region starts from {0:5.1f}ps (max(1-ks)={1:5.3g} continuity={2:5.1f}ps)".format(x[-1] - maxContCluster[0], max(maxContKS), maxContinuity)
+    last_point_above_threshold = x[-1]-minimum_sampling_time
+    print "Proportion of simulation above convergence threshold: {0:5.3f}".format((last_point_above_threshold-equilibration_time)/(x[-1]-x[0]))
+    print "Minimum ks statistic in equilibrated region: {0:5.3g}".format(largest_converged_block_minimum_ks_value)
+    print "Minimum p value in equilibrated region: {0:5.3g}".format(largest_converged_block_minimum_p_value)
+    print
 
-    startIndex = value_to_closest_index(x, x[-1] - maxContCluster[0])
-    x_converged = x[startIndex:]
-    y_converged = y[startIndex:]
